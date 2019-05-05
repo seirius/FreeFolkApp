26609f916129b6edab54d3ffe8e87ea53099056e
Andriy <andsei91@gmail.com>
2019-05-05 17:28:05 +0200

    Moar awesomness

--- src/preload.js
+++ src/preload.js
@@ -5,7 +5,7 @@ const ffmpeg = require("fluent-ffmpeg");
 const {google} = require("googleapis");
 const youtube = google.youtube("v3");
 const CREDENTIALS = require("../FreefolkCredentials.json");
-const youtubedl = require("@microlink/youtube-dl");
+const ytdl = require("ytdl-core");
 
 function getPlaylist(url) {
     return new Promise((resolve, reject) => {
@@ -35,14 +35,90 @@ function getPlaylist(url) {
     });
 }
 
+function getBasicInfo(args) {
+    return new Promise((resolve, reject) => {
+        const {videoUrl} = args;
+        ytdl(videoUrl, (err, info) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(info);
+            }
+        });
+    });
+}
+
+function downloadVideo(args) {
+    return new Promise((resolve, reject) => {
+        const {videoUrl, videoTitle, savePath} = args;
+        const vid = ytdl(videoUrl);
+        vid.pipe(fs.createWriteStream(path.join(savePath, safeFilename(videoTitle) + ".mp4")));
+        vid.on("response", resolve).on("error", reject);
+    });
+}
+
+function downloadMusic(args) {
+    return new Promise((resolve, reject) => {
+        const {savePath, videoTitle, videourl} = args;
+        downloadVideo(args)
+        .then(response => {
+            const fileName = safeFilename(videoTitle);
+            const videoPath = path.join(savePath, `${fileName}.mp4`);
+            const mp3Path = path.join(savePath, `${fileName}.mp3`);
+            ffmpeg(videoPath)
+            .format("mp3")
+            .save(mp3Path)
+            .on("end", () => fs.unlink(videoPath, err => err ? reject(err) : resolve()))
+            .on("error", reject);
+        }).catch(reject)
+    });
+}
+
+function safeFilename(oFilaname) {
+    return oFilaname.replace(/[^a-z0-9]/gi, '_').toLowerCase();
+}
+
+function removeItems(args) {
+    const {filePaths} = args;
+    const promises = [];
+    filePaths.forEach(filePath => {
+        promises.push(new Promise((resolve, reject) => {
+            fs.exists(filePath, exists => {
+                if (exists) {
+                    fs.unlink(filePath, err => err ? reject(err) : resolve());
+                } else {
+                    resolve();
+                }
+            });
+        }));
+    });
+    console.log(promises);
+    return Promise.all(promises);
+}
+
+function removeVideosContent(args) {
+    const {savePath, videoTitles, fileTypes} = args;
+    const filePaths = [];
+    videoTitles.map(videoTitle => {
+        const safeTitle = safeFilename(videoTitle);
+        fileTypes.forEach(fileType => filePaths.push(path.join(savePath, safeTitle + fileType)));
+    });
+    return removeItems({filePaths});
+}
+
+
 window.electron = {
     dialog,
     fs,
     path,
     ffmpeg,
     google: {
-        getPlaylist
+        getPlaylist,
+        getBasicInfo,
+        downloadVideo,
+        downloadMusic,
+        removeItems,
+        removeVideosContent
     },
-    youtubedl,
     setImmediate
 };
