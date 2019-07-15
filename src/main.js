074cca59927ae843137f6d9bff4e08d6f2d3e851
Andriy Yednarovych <andsei91@gmail.com>
2019-07-15 13:33:40 +0200

    Frameless

--- src/main.js
+++ src/main.js
@@ -9,7 +9,8 @@ function createWindow() {
         height: 600,
         webPreferences: {
             preload: path.join(__dirname, "preload.js")
-        }
+        },
+        frame: false
     });
 
     win.loadFile('web-dist/index.html');
