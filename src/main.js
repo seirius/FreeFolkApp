e31205b2880f5851c357f46692ad4e4e00ae6199
Andriy Yednarovych <andsei91@gmail.com>
2019-07-23 10:34:39 +0200

    test-autoupdate

--- src/main.js
+++ src/main.js
@@ -3,6 +3,35 @@ const path = require("path");
 
 if (require("electron-squirrel-startup")) return;
 
+const os = require('os').platform();
+
+if (os === "win32") {
+    const { autoUpdater } = require("electron-updater");
+
+    autoUpdater.on('checking-for-update', () => {
+        console.log('Checking for update...');
+    });
+    autoUpdater.on('update-available', (info) => {
+        console.log('Update available.');
+    });
+    autoUpdater.on('update-not-available', (info) => {
+        console.log('Update not available.');
+    });
+    autoUpdater.on('error', (err) => {
+        console.log('Error in auto-updater. ' + err);
+    });
+    autoUpdater.on('download-progress', (progressObj) => {
+        let log_message = "Download speed: " + progressObj.bytesPerSecond;
+        log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+        log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+        console.log(log_message);
+    });
+    autoUpdater.on('update-downloaded', (info) => {
+        console.log('Update downloaded');
+    });
+}
+
+
 function createWindow() {
     let win = new BrowserWindow({
         width: 1200,
@@ -20,11 +49,13 @@ function createWindow() {
             const isVideo = url.indexOf("/mp4") > -1;
             let width = isVideo ? 1300 : 300;
             let height = isVideo ? 820 : 100;
-            Object.assign(options, {width, height});
+            Object.assign(options, { width, height });
         }
-    })
+    });
 
-    // win.webContents.openDevTools();
+    if (os === "win32") {
+        autoUpdater.checkForUpdatesAndNotify();
+    }
 }
 
 app.on('ready', createWindow);
